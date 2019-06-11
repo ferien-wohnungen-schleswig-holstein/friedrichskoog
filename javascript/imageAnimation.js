6e6eeb80a3435d3f497a29e5a56953ca59bbf210
Moritz Fortmann <moritz.fortmann@yahoo.de>
2019-06-11 20:05:54 +0200

    add mobile wohnungsübersicht

---
+++ javascript/imageAnimation.js
@@ -0,0 +1,29 @@
+var animateHTML = function() {
+    var elems;
+    var windowHeight;
+    function init() {
+      elems = document.querySelectorAll('.animateHidden');
+      windowHeight = window.innerHeight;
+      addEventHandlers();
+      checkPosition();
+    }
+    function addEventHandlers() {
+      window.addEventListener('scroll', checkPosition);
+      window.addEventListener('resize', init);
+    }
+    function checkPosition() {
+      for (var i = 0; i < elems.length; i++) {
+        var positionFromTop = elems[i].getBoundingClientRect().top;
+        if (positionFromTop - windowHeight <= -380) {
+          elems[i].className = elems[i].className.replace(
+            'animateHidden',
+            'animate'
+          );
+        }
+      }
+    }
+    return {
+      init: init
+    };
+  };
+  animateHTML().init();
