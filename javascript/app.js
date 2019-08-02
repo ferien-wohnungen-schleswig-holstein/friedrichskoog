c4ecf20cd25559360f3cb44da22bb93abe6d5edd
Moritz Fortmann <moritz.fortmann@yahoo.de>
2019-08-02 20:42:23 +0200

    something pwa done?

--- javascript/app.js
+++ javascript/app.js
@@ -2,4 +2,6 @@ if('serviceWorker' in navigator){
     navigator.serviceWorker.register('/sw.js')
       .then(reg => console.log('service worker registered'))
       .catch(err => console.log('service worker not registered', err));
+  }else{
+    console.log('not in navigator');
   }
